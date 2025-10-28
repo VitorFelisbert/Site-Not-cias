563da93477a12a0c51b3a5707cfa8f7ac2c97ffa
VitorFelisbert <vitorafs22@gmail.com>
2025-10-28 19:50:46 -0300

    reajuste

--- js/noticia.js
+++ js/noticia.js
@@ -1,3 +1,14 @@
 const urlParams = new URLSearchParams(window.location.search)
 const urlNoticia = urlParams.get('id');
-document.querySelector('#titulo').innerHTML = urlNoticia;
+document.querySelector('#LerMais').href = urlNoticia;
+
+fetch("./../bancoDados.json").then(response => response.json()).then(data => {
+    const noticiaUrl= urlNoticia;
+    const artigo= data.articles.find((article) => article.url === noticiaUrl);
+    document.querySelector("#titulo").innerHTML=artigo.title;
+    document.querySelector("#author").innerHTML=artigo.author;
+    document.querySelector("#imagem").src=artigo.urlToImage;
+    document.querySelector("#conteudo").innerHTML=artigo.description;
+
+}
+)
