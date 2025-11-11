d0427160eb3f7dcf1f1a18a510677dbd1e9c6eb8
VitorFelisbert <vitorafs22@gmail.com>
2025-11-11 19:47:43 -0300

    finalizando site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
- fetch("https://newsapi.org/v2/everything?q=brasil&from=2025-10-27&sortBy=publishedAt&apiKey=366a4893cc3440b486f45b01e7fe90a5").then(response => response.json()).then(data => {
+ const data = new Date();
+ const ano = String(data.getFullYear());
+ const mes = String(data.getMonth()+1).padStart(2,'0');
+ const dia = String(data.getDate()-1).padStart(2,'0');
+ const diaAnterior = ano+'-'+mes+'-'+dia;
+ 
+ 
+ //fetch(`https://newsapi.org/v2/everything?q=brasil&from=${diaAnterior}&sortBy=publishedAt&apiKey=366a4893cc3440b486f45b01e7fe90a5`).then(response => response.json()).then(data => {
     //const noticiaUrl= 'https://exemplo.com/noticia2';
     //const artigo= data.articles.find((article) => article.url === noticiaUrl);
     //console.log(artigo);
+    fetch(`bancoDados.json`).then(response => response.json()).then(data => {
     const container = document.createElement('div');
     data.articles .forEach( article =>
     {
@@ -12,7 +20,7 @@
         <p>${article.author}</p>
         <img src="${article.urlToImage}">
         </a>
-        `
+        `;
         container.appendChild(noticia);
 
     }
